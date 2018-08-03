7296ff0c2ab6fec7d50e7fb44811770dbeb1eb7b
Perrym14 <malikap14@gmail.com>
2018-08-03 19:42:59 -0400

    Added WorldFactory class in order to generate new locations for the game.

---
+++ Engine/Factories/WorldFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    internal class WorldFactory
+    {
+        internal World CreateWorld()
+        {
+            World newWorld = new World();
+
+            newWorld.AddLocation(0, -1, "Home", "This is your home.", "/Engine;component/Images/Locations/Home.png");
+
+            return newWorld;
+        }
+    }
+}
--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Engine.Models;
+using Engine.Factories;
 
 namespace Engine.ViewModels
 {
     public class GameSession
     {
-       public Player CurrentPlayer { get; set; }
+        public World CurrentWorld { get; set; }
+        public Player CurrentPlayer { get; set; }
         public Location CurrentLocation { get; set; }
+
 
         public GameSession()
         {
@@ -22,13 +25,11 @@ namespace Engine.ViewModels
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
 
-            CurrentLocation = new Location();
-            CurrentLocation.Name = "Home";
-            CurrentLocation.XCoordinate = 0;
-            CurrentLocation.YCoordinate = -1;
-            CurrentLocation.Description = "This is your house.";
-            CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
-            
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory.CreateWorld();
+
+            CurrentLocation = CurrentWorld.LocationAt(0, -1);
+
         }
 
     }
