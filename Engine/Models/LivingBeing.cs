3d1de32e7d394bb9972cb53291f0018123e16eea
Perrym14 <malikap14@gmail.com>
2018-08-11 14:37:10 -0400

    Bug fix: Upon completion of quest, non-unique items required for quest completion are now completely removed from player's inventory.

--- Engine/Models/LivingBeing.cs
+++ Engine/Models/LivingBeing.cs
@@ -141,7 +141,9 @@ namespace Engine.Models
         {
             Inventory.Remove(item);
 
-            GroupedInventoryItem groupedInventoryItemToRemove = GroupedInventory.FirstOrDefault(gi => gi.Item == item);
+            GroupedInventoryItem groupedInventoryItemToRemove = item.IsUnique ?
+                GroupedInventory.FirstOrDefault(gi => gi.Item == item) :
+                GroupedInventory.FirstOrDefault(gi => gi.Item.ItemTypeID == item.ItemTypeID);
 
             if (groupedInventoryItemToRemove != null)
             {
